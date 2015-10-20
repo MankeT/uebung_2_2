b032b0e17e927ebe641cbdd83d3ba74276ecd9ac
MankeT <tim.manke@nordakademie.de>
2015-10-20 11:05:44 +0200

    3. Projekt zur Lösung der Binomischen Formel.

---
+++ src/uebung_2_2/BinomischeFormel.java
@@ -0,0 +1,16 @@
+package uebung_2_2;
+
+public class BinomischeFormel {
+
+	public static void main(String[] args) {
+		
+		double a = 1;
+		double b = 2;
+		
+		double binomi = (a+b)*(a+b);
+		
+		System.out.println("Lösung für erste Binomische Formel für a = " + a + " für b = " + b + " ist " + binomi + ".");
+
+	}
+
+}
